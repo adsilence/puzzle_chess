dba19b04af83d814ee5e4d306fab027d35051958
Alex Silence <asilence14@gmail.com>
2024-07-28 08:09:48 -0400

    Base commit

---
+++ pieceMove.js
@@ -0,0 +1,20 @@
+function getValidMovesPawn(x,y,board) {
+
+    return [[0,0], [0,2]];
+}
+
+function getValidMovesKnight(x,y,board) {
+
+}
+function getValidMovesBishop(x,y,board) {
+
+}
+function getValidMovesRook(x,y,board) {
+
+}
+function getValidMovesQueen(x,y,board) {
+
+}
+function getValidMovesKing(x,y,board) {
+
+}
---
+++ spritesheet.js
@@ -0,0 +1,77 @@
+export const atlasData = {
+    frames: {
+        king_black: {
+            frame: { x:0, y:0, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        queen_black: {
+            frame: { x:128, y:0, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        bishop_black: {
+            frame: { x:256, y:0, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        knight_black: {
+            frame: { x:384, y:0, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        rook_black: {
+            frame: { x:512, y:0, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        pawn_black: {
+            frame: { x:640, y:0, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+
+        king_white: {
+            frame: { x:0, y:128, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        queen_white: {
+            frame: { x:128, y:128, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        bishop_white: {
+            frame: { x:256, y:128, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        knight_white: {
+            frame: { x:384, y:128, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        rook_white: {
+            frame: { x:512, y:128, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+        pawn_white: {
+            frame: { x:640, y:128, w:128, h:128 },
+            sourceSize: { w:128, h:128 },
+            spriteSourceSize: { x:0, y:0, w:128, h:128}
+        },
+
+        board_piece: {
+            frame: { x:768, y:0, w:256, h:256 },
+            sourceSize: { w:256, h:256 },
+            spriteSourceSize: { x:0, y:0, w:256, h:256}
+        },
+    },
+    meta: {
+        image: 'spritesheet.png',
+        format: 'RGBA8888',
+        size: { w:1024, h:1024 },
+        scale: 1
+    }
+}
