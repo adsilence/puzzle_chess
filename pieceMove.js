8a808e47a8200657c48fa2c772ad24fba46a20e4
Alex Silence <asilence14@gmail.com>
2024-07-29 13:28:09 -0400

    Added piece clicking and move logic base

--- pieceMove.js
+++ pieceMove.js
@@ -1,20 +1,41 @@
+export function getValidMoves(x,y,board,piece) {
+    if (piece === "pawn_black" || piece === "pawn_white") {
+        return getValidMovesPawn(x,y,board);
+    }
+    if (piece === "knight_black" || piece === "knight_white") {
+        return getValidMovesKnight(x,y,board);
+    }
+    if (piece === "bishop_black" || piece === "bishop_white") {
+        return getValidMovesBishop(x,y,board);
+    }
+    if (piece === "rook_black" || piece === "rook_white") {
+        return getValidMovesRook(x,y,board);
+    }
+    if (piece === "queen_black" || piece === "queen_white") {
+        return getValidMovesQueen(x,y,board);
+    }
+    if (piece === "king_black" || piece === "king_white") {
+        return getValidMovesKing(x,y,board);
+    }
+}
+
 function getValidMovesPawn(x,y,board) {
 
     return [[0,0], [0,2]];
 }
 
 function getValidMovesKnight(x,y,board) {
-
+    return [[x + 1, y + 2], [x + 2, y + 1], [x - 1, + y + 2], [x - 2, y + 1]];
 }
 function getValidMovesBishop(x,y,board) {
-
+    return [];
 }
 function getValidMovesRook(x,y,board) {
-
+    return [];
 }
 function getValidMovesQueen(x,y,board) {
-
+    return [];
 }
 function getValidMovesKing(x,y,board) {
-
+    return [];
 }
